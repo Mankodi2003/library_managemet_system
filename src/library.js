9bfab2f2ae840b5e24393fac6704a8ed93478feb
prasiddh <prasiddh@example.com>
2024-08-31 14:46:28 +0530

    Implement return book feature

--- src/library.js
+++ src/library.js
@@ -19,6 +19,24 @@ class Library {
         }
         book.available = false;
       }
+
+      borrowBook(isbn) {
+        const book = this.books.find((b) => b.isbn === isbn && b.available);
+        if (!book) {
+          throw new Error('Book is not available');
+        }
+        book.available = false;
+      }
+
+
+      returnBook(isbn) {
+        const book = this.books.find((b) => b.isbn === isbn && !b.available);
+        if (!book) {
+          throw new Error('Book was not borrowed');
+        }
+        book.available = true;
+      }
+      
       
   }
   
